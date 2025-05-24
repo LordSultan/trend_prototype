a848088e88dba0d10e2c1c292b0dd528f9f8734e
LordSultan <sultansaryah@gmail.com>
2025-05-24 19:29:32 +0300

    fixed an issue with the server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-backend-api.onrender.com/api'  // Replace with your actual API URL
+  // apiUrl: 'https://your-backend-api.onrender.com/api'  // Replace with your actual API URL
+  apiUrl: 'https://trend-prototype.onrender.com/api'  // Replace with your actual API URL
 };
